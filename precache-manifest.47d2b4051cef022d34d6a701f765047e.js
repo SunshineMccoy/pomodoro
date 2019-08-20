054f17e35620cc64cc11b072e44a86e9a37c864b
Michael McCoy <mccoymichaeljohn@gmail.com>
2019-08-19 21:49:06 -0400

    Updates

---
+++ precache-manifest.47d2b4051cef022d34d6a701f765047e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5478d79362772b6794ded1e7936f6a",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "1306afe28295b43fa2dd",
+    "url": "/pomodoro/static/css/main.ebfc7ca7.chunk.css"
+  },
+  {
+    "revision": "48ee9992ffc8e8a96576",
+    "url": "/pomodoro/static/js/2.a4300ecd.chunk.js"
+  },
+  {
+    "revision": "1306afe28295b43fa2dd",
+    "url": "/pomodoro/static/js/main.c0553a39.chunk.js"
+  },
+  {
+    "revision": "0cf99d9e6445e1215f2c",
+    "url": "/pomodoro/static/js/runtime~main.ad7a03aa.js"
+  }
+]);
